fc7d30b5666473a63a53bc67f5b1a24a816d9572
Thanh Khang <nthanhkhang@outlook.com>
2021-01-21 21:50:30 +0700

    tungnt

---
+++ 2 - Strategy/khangnt_518H0372_lab02_ws/tungnt_51800649_lab02_ws/Lab02_01_Strategy/src/main/java/Lab02_01/dp/tdtu/Main.java
@@ -0,0 +1,11 @@
+package Lab02_01.dp.tdtu;
+
+public class Main {
+
+	public static void main(String[] args) {
+		StudentList test1 = DataBase.init();
+		// TODO Auto-generated method stub
+
+	}
+
+}
